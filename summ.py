730a935b8f2520131c753ad1d0e9c64c91927b47
Anna-Dar <annadarbinyan28@gmail.com>
2026-03-05 10:56:42 +0300

    Update summ.py

--- summ.py
+++ summ.py
@@ -3,7 +3,7 @@ num1 = float(input("Введите первую цифру: "))
 num2 = float(input("Введите вторую цифру: "))
 
 
-sum_result = num1 + num2
+sum_result = num1 - num2
 
 
-print("Сумма:", sum_result)
+print("Разность:", sum_result)
